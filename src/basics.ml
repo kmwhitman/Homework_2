ee1bc47771ad9277fc034cfad3ce659a49196352
kmwhitman <kwhitm9@lsu.edu>
2025-09-30 20:28:31 -0500

    Completed parts 1, 2 and 3.

--- src/basics.ml
+++ src/basics.ml
@@ -6,28 +6,42 @@ let abs x =
 (* Part 1: Non-Recursive Functions *)
 (***********************************)
 
-let rev_tup (tup : 'a * 'b) = failwith "unimplemented"
-let rev_triple (tup : 'a * 'b * 'c) = failwith "unimplemented"
+let rev_tup (tup : 'a * 'b) = (b, a)
+let rev_triple (tup : 'a * 'b * 'c) = (c, b, a)
 
-let is_odd x = failwith "unimplemented"
+let is_odd x = x mod 2<> 0
 
-let is_older (date1: int * int * int) (date2: int * int * int) = failwith "unimplemented"
+let is_older (date1: int * int * int) (date2: int * int * int) = if y1 < y2 then true
+                                                                  else if y1 > y2 then false
+                                                                  else if m1 < m2 then true
+                                                                  else if m1 > m2 then false
+                                                                  else d1<d2
 
-let to_us_format (date1: int * int * int) = failwith "unimplemented"
+let to_us_format (date1: int * int * int) = (month, day, year)
 (*******************************)
 (* Part 2: Recursive Functions *)
 (*******************************)
 
-let rec pow x p = failwith "unimplemented"
+let rec pow x p = if p = 0 then 1
+                  else x * pow x (p-1)
 
-let rec fac n = failwith "unimplemented"
+let rec fac n =   if n = 1 then 1
+                  else n * fac (n - 1)
 
 (*****************)
 (* Part 3: Lists *)
 (*****************)
 
-let rec get_nth ((idx:int), (lst: 'a list)) = failwith "unimplemented"
+let rec get_nth (idx:int)= 
+    match lst with
+    | [] -> failwith "Index out of bounds"
+    | hd :: tl -> if idx = 0 then hd else get_nth (idx - 1, t1)
 
-let larger lst1 lst2 = failwith "unimplemented"
+let larger lst1 lst2 = let len1 = List.length lst1 in
+                       let len 2 = List.length lst2 in
+                       if len1 > len2 then lst1
+                       else if len2 > len1 then lst2  
+                       else []
 
-let sum lst1 lst2 = failwith "unimplemented"
+let sum lst1 lst2 = let sum_list 1 = List.fold_left (fun acc x -> acc + x) 0 1 in 
+                    sum_list 1st1 + sum_list 1st2
